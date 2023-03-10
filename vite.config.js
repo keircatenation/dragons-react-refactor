650b86dc6303e5ff9a7f8d1f0c7697f03dbfd23d
Keiran Pillman <keiran.pillman@gmail.com>
2023-03-09 19:46:44 -0500

    adding the docs config to vits config file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/dragons-react-refactor/"
+  base: "/dragons-react-refactor/",
+  build: {
+    outDir: 'docs'
+  }
 })
